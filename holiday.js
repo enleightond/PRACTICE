c16bdc74739ab072b9a3f4407a7789de3ea39216
enleightond <enleightond@yahoo.com>
2015-12-30 16:04:34 -0800

    finished first Objects and arrays practice

--- holiday.js
+++ holiday.js
@@ -1,7 +1,26 @@
-var list = ['red', 'green', 'blue'];
+// var list = ['red', 'green', 'blue'];
 
-function upperCase(str){
-	return upperCase().prototype.toUpperCase();
-}
+// function upperCase(str){
+// 	return str.toUpperCase();
+// }
 
-console.log(upperCase([list]));
+// console.log(list.map(upperCase));
+
+//**************************************
+
+// var list = [
+//   {width: 20, color: 'red'},
+//   {width: 50, color: 'blue'},
+//   {width: 10, color: 'purple'}
+// ];
+
+// function getColors(arr){
+// 	for (var i = 0; i<arr.length; i++){
+// 		console.log(list[i].color);
+// 	}
+// }
+
+// console.log(getColors(list));
+
+//////////////////////////////////////////
+
