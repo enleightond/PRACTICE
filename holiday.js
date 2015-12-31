af35c1fb20ae4e67460bda730be62e6ca278eecb
enleightond <enleightond@yahoo.com>
2015-12-31 14:28:43 -0800

    started working on the orc functions

--- holiday.js
+++ holiday.js
@@ -1,3 +1,6 @@
+//Write a function that returns each string in this array in all uppercase letters.
+///////////////////////////////////////////////////////////////////////////////////
+
 // var list = ['red', 'green', 'blue'];
 
 // function upperCase(str){
@@ -6,7 +9,8 @@
 
 // console.log(list.map(upperCase));
 
-//**************************************
+//Write a function that returns the colors for every object in the array.
+/////////////////////////////////////////////////////////////////////////
 
 // var list = [
 //   {width: 20, color: 'red'},
@@ -22,5 +26,76 @@
 
 // console.log(getColors(list));
 
-//////////////////////////////////////////
+//Write a function that logs everything in an array.
+////////////////////////////////////////////////////
+
+// var list = ['milk', 'butter', 'cheese'];
+
+// function dairy(str){
+// 	for (var i = 0; i < str.length; i++){
+// 	console.log(str[i]);		
+// 	}
+// }
+
+// console.log(dairy(list));
+
+//Write a function that logs the first and last item in an array.
+//////////////////////////////////////////////////////////////////
+
+// var list = ['milk', 'butter', 'cheese'];
+
+// function firstLast(arr){
+// 		console.log(arr[0]);
+// 		console.log(arr.slice(-1)[0]);
+// }
+// console.log(firstLast(list));
+
+//Write a function that returns each string in this array in all lowercase letters.
+///////////////////////////////////////////////////////////////////////////////////
+
+// var list = ['RED', 'GREEN', 'BLUE'];
+
+// function toLower(str){
+// 	for (var i = 0; i < str.length; i++) {
+// 			console.log(str[i].toLowerCase());
+// 		}
+// }
+// console.log(toLower(list));
+
+//Write a function that logs the object with the greatest strength attribute.
+//////////////////////////////////////////////////////////////////////////////
+
+var orcs = [
+    {name: 'Orgoth', strength: 9001},
+    {name: 'Blaroguhh', strength: 500},
+    {name: 'Mark', strength: 543}
+];
+
+function getStrongest(orc){
+	var strongest = 0;
+	for (var i = 0; i < orc.length; i++){
+		if (orcs[i].strength > strongest) {
+			strongest === orcs.strength;
+		}
+	}
+}
+
+console.log(getStrongest(orcs));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- warmup.js
+++ warmup.js
@@ -50,6 +50,33 @@ function promisifyGet(url) {
 	}); //end of the promise constructor
 }
 
+//***************************************************
+var second = [1,2,3].map(function(item) {
+	return item + 1;
+}); 
+
+console.log(second);
+
+var input = [
+		{1: 'a'},
+		{2: 'b'},
+		{3: 'c'},
+		{4: 'd'}
+
+
+];
+input.map(function(swap){
+	var newObj = {};
+	for (var prop in input) {
+		if (swap.hasOwnProperty(swap)){
+			newObj[input[prop]] = prop;
+		}
+	}
+	return newObj;
+});
+console.log(newObj);
+
+
 
 
 
